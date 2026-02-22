a20a128c6312bc48ddc2a2aaf59747d91deab8f3
andreev2007 <andreevdaniil564@gmail.com>
2026-02-22 09:51:40 +0300

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -8,4 +8,9 @@ int main() {
 
   // TODO: Uncomment the code below to pass the first stage
    std::cout << "$ ";
+   std::string userCommand;
+
+   std::getline(std::cin, userCommand);
+
+   std::cout << userCommand << ": command not found" << std::endl;
 }
