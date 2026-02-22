695f07fa90eedc902a07661b7d86f19c400b489d
andreev2007 <andreevdaniil564@gmail.com>
2026-02-22 11:40:48 +0300

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -13,11 +14,24 @@ int main() {
 
 	std::getline(std::cin >> std::ws, userCommand);
 
+	std::stringstream ss(userCommand);
+
 	if (userCommand == "exit") {
 		break;
 	}
 
-	std::cout << userCommand << ": command not found" << std::endl;
+	std::string firstWord;
+	ss >> firstWord;
+
+	size_t space_pos = userCommand.find(' ');
+
+	if (space_pos != std::string::npos && firstWord == "echo") {
+		userCommand.erase(0, space_pos + 1);
+		std::cout << userCommand << std::endl;
+	}
+	else {
+		std::cout << userCommand << ": command not found" << std::endl;
+	}
   }
   return 0;
 }
