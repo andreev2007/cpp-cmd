d6e5d0de8b4d69a6f58d3f6cc6acf419edebf683
andreev2007 <andreevdaniil564@gmail.com>
2026-02-23 18:53:12 +0300

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,53 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <sstream>
+#include <algorithm>
+
+class Command {
+private:
+	std::string userCommand;
+	const std::vector<std::string> commands {"echo", "type"};
+
+public:
+	Command() {
+		std::getline(std::cin >> std::ws, userCommand);
+	};
+
+	int execute() {
+		std::stringstream ss(userCommand);
+
+		if (userCommand == "exit") {
+			return 1;
+		}
+
+		std::string firstWord;
+		ss >> firstWord;
+
+		size_t space_pos = userCommand.find(' ');
+		bool hasPos = space_pos != std::string::npos;
+
+		if (hasPos && firstWord == "echo") {
+			userCommand.erase(0, space_pos + 1);
+			std::cout << userCommand << std::endl;
+		}
+		else if (hasPos && firstWord == "type") {
+			userCommand.erase(0, space_pos + 1);
+			auto it = std::find(commands.begin(), commands.end(), userCommand);
+
+			if (it != commands.end()) {
+				std::cout << userCommand << " is a shell builtin" << std::endl;
+			}
+			else {
+				std::cout << userCommand << ": not found" << std::endl;
+			}
+		}
+		else {
+			std::cout << userCommand << ": command not found" << std::endl;
+		}
+		return 0;
+	}
+};
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -10,27 +57,10 @@ int main() {
   // TODO: Uncomment the code below to pass the first stage
   while (true) {
 	std::cout << "$ ";
-	std::string userCommand;
-
-	std::getline(std::cin >> std::ws, userCommand);
-
-	std::stringstream ss(userCommand);
-
-	if (userCommand == "exit") {
+	Command* command = new Command();
+	int executionStatus = command->execute();
+	if (executionStatus) {
 		break;
-	}
-
-	std::string firstWord;
-	ss >> firstWord;
-
-	size_t space_pos = userCommand.find(' ');
-
-	if (space_pos != std::string::npos && firstWord == "echo") {
-		userCommand.erase(0, space_pos + 1);
-		std::cout << userCommand << std::endl;
-	}
-	else {
-		std::cout << userCommand << ": command not found" << std::endl;
 	}
   }
   return 0;
