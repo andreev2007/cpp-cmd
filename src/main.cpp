0cffe26264dd3debdaf059c4793859528d584bbd
andreev2007 <andreevdaniil564@gmail.com>
2026-03-04 11:11:49 +0300

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -113,7 +113,6 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
   while (true) {
 	std::cout << "$ ";
 	Command* command = new Command();
